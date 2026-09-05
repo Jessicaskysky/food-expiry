7554f20525583a25668bd63d22495ee3fc9e8d78
Jessicaskysky <jblue1023@gmail.com>
2026-09-05 22:02:49 +0800

    fix: service worker 改為 network-first，確保 app 每次開啟拿最新版

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'food-tracker-v4';
+const CACHE = 'food-tracker-v5';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
@@ -17,6 +17,12 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   );
 });
